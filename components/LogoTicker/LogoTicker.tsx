2e7ac3d301f121d5510800a15a2be1db1aa70a71
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2024-11-21 13:20:44 +0600

    added animation in LogoTicker section

--- components/LogoTicker/LogoTicker.tsx
+++ components/LogoTicker/LogoTicker.tsx
@@ -1,11 +1,13 @@
+"use client";
+
 import React from "react";
-import Image from "next/image";
 import acmeLogo from "@/public/logo-acme.png";
 import apexLogo from "@/public/logo-apex.png";
 import celestialLogo from "@/public/logo-celestial.png";
 import echoLogo from "@/public/logo-echo.png";
 import pulseLogo from "@/public/logo-pulse.png";
 import quantumLogo from "@/public/logo-quantum.png";
+import { motion } from "framer-motion";
 
 const LogoTicker = () => {
   return (
@@ -15,8 +17,17 @@ const LogoTicker = () => {
           <div className="flex-1 md:flex-none">
             <h2>Trusted by top innovative teams</h2>
           </div>
-          <div className="flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
-            <div className="flex flex-none gap-14">
+          <div className="flex flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+            <motion.div
+              initial={{ translateX: "-50%" }}
+              animate={{ translateX: "0" }}
+              transition={{
+                repeat: Infinity,
+                duration: 30,
+                ease: "linear",
+              }}
+              className="flex flex-none gap-14 pr-14 -translate-x-1/2"
+            >
               {[
                 acmeLogo,
                 pulseLogo,
@@ -24,15 +35,21 @@ const LogoTicker = () => {
                 celestialLogo,
                 apexLogo,
                 quantumLogo,
-              ].map((logo) => (
-                <Image
+                acmeLogo,
+                pulseLogo,
+                echoLogo,
+                celestialLogo,
+                apexLogo,
+                quantumLogo,
+              ].map((logo, index) => (
+                <img
                   src={logo.src}
-                  key={logo.src}
+                  key={`${logo.src}-${index}`}
                   alt={logo.src}
-                  className="w-auto h-6 mr-4"
+                  className="w-auto h-6 flex-none"
                 />
               ))}
-            </div>
+            </motion.div>
           </div>
         </div>
       </div>
