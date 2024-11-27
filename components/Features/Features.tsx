4103ef70becd45788cc9892d1e107fadfd50731c
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2024-11-28 00:43:59 +0600

    added animation in features section

--- components/Features/Features.tsx
+++ components/Features/Features.tsx
@@ -1,6 +1,11 @@
 "use client";
 
-import React, { useEffect, useRef } from "react";
+import React, {
+  ComponentPropsWithoutRef,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import {
   DotLottieCommonPlayer,
   DotLottiePlayer,
@@ -41,7 +46,12 @@ const tabs = [
   },
 ];
 
-const FeatureTab = (tab: (typeof tabs)[number]) => {
+const FeatureTab = (
+  props: (typeof tabs)[number] &
+    ComponentPropsWithoutRef<"div"> & { selected: boolean }
+) => {
+  const tabRef = useRef<HTMLDivElement>(null);
+
   const dotLottieRef = useRef<DotLottieCommonPlayer>(null);
 
   const xPercentage = useMotionValue(0);
@@ -50,7 +60,24 @@ const FeatureTab = (tab: (typeof tabs)[number]) => {
   const maskImage = useMotionTemplate`radial-gradient(80px 80px at ${xPercentage}% ${yPercentage}%, black, transparent)`;
 
   useEffect(() => {
+    if (!tabRef.current || !props.selected) return;
+
+    xPercentage.set(0);
+    yPercentage.set(0);
+
+    const { width, height } = tabRef.current?.getBoundingClientRect();
+    const circumference = height * 2 + width * 2;
+
+    const times = [
+      0,
+      width / circumference,
+      (width + height) / circumference,
+      (width * 2 + height) / circumference,
+      1,
+    ];
+
     const options: ValueAnimationTransition = {
+      times,
       duration: 4,
       repeat: Infinity,
       ease: "linear",
@@ -59,7 +86,7 @@ const FeatureTab = (tab: (typeof tabs)[number]) => {
 
     animate(xPercentage, [0, 100, 100, 0, 0], options);
     animate(yPercentage, [0, 0, 100, 100, 0], options);
-  }, []);
+  }, [props.selected]);
 
   const handleTabHover = () => {
     if (dotLottieRef.current === null) return;
@@ -69,25 +96,29 @@ const FeatureTab = (tab: (typeof tabs)[number]) => {
 
   return (
     <div
+      ref={tabRef}
       onMouseEnter={handleTabHover}
       className="border border-white/15 relative flex gap-2.5 items-center lg:flex-1 p-2.5 rounded-xl"
+      onClick={props.onClick}
     >
-      <motion.div
-        style={{
-          maskImage,
-        }}
-        className="absolute inset-0 -m-px rounded-xl border border-[#a369ff]"
-      ></motion.div>
+      {props.selected && (
+        <motion.div
+          style={{
+            maskImage,
+          }}
+          className="absolute inset-0 -m-px rounded-xl border border-[#a369ff]"
+        ></motion.div>
+      )}
       <div className="w-12 h-12 border border-white/15 rounded-lg inline-flex items-center justify-center">
         <DotLottiePlayer
           ref={dotLottieRef}
-          src={tab.icon}
+          src={props.icon}
           className="w-5 h-5"
           autoplay
         />
       </div>
-      <div className="font-medium">{tab.title}</div>
-      {tab.isNew && (
+      <div className="font-medium">{props.title}</div>
+      {props.isNew && (
         <div className="text-xs rounded-full px-2 py-0.5 bg-[#8c44ff] text-black font-semibold">
           new
         </div>
@@ -97,6 +128,42 @@ const FeatureTab = (tab: (typeof tabs)[number]) => {
 };
 
 const Features = () => {
+  const [selectedTab, setSelectedTab] = useState(0);
+
+  const backgroundPositionX = useMotionValue(tabs[0].backgroundPositionX);
+  const backgroundPositionY = useMotionValue(tabs[0].backgroundPositionY);
+  const backgroundSizeX = useMotionValue(tabs[0].backgroundSizeX);
+
+  const backgroundPosition = useMotionTemplate`${backgroundPositionX}% ${backgroundPositionY}%`;
+  const backgroundSize = useMotionTemplate`${backgroundSizeX}% auto`;
+
+  const handleSelectTab = (index: number) => {
+    setSelectedTab(index);
+
+    const animateOptions: ValueAnimationTransition = {
+      duration: 2,
+      ease: "easeInOut",
+    };
+
+    animate(
+      backgroundSizeX,
+      [backgroundSizeX.get(), 100, tabs[index].backgroundSizeX],
+      animateOptions
+    );
+
+    animate(
+      backgroundPositionX,
+      [backgroundPositionX.get(), tabs[index].backgroundPositionX],
+      animateOptions
+    );
+
+    animate(
+      backgroundPositionY,
+      [backgroundPositionY.get(), tabs[index].backgroundPositionY],
+      animateOptions
+    );
+  };
+
   return (
     <section className="py-20 md:py-24">
       <div className="container">
@@ -108,15 +175,24 @@ const Features = () => {
           revolutionized the way businesses approach SEO.
         </p>
         <div className="mt-10 flex flex-col lg:flex-row gap-3">
-          {tabs.map((tab) => (
-            <FeatureTab {...tab} key={tab.title} />
+          {tabs.map((tab, tabIndex) => (
+            <FeatureTab
+              {...tab}
+              selected={selectedTab === tabIndex}
+              onClick={() => handleSelectTab(tabIndex)}
+              key={tab.title}
+            />
           ))}
         </div>
         <div className="border border-white/20 p-2.5 rounded-xl mt-3">
-          <div
+          <motion.div
             className="aspect-video bg-cover border border-white/20 rounded-lg"
-            style={{ backgroundImage: `url(${productImage.src})` }}
-          ></div>
+            style={{
+              backgroundPosition,
+              backgroundSize,
+              backgroundImage: `url(${productImage.src})`,
+            }}
+          ></motion.div>
         </div>
       </div>
     </section>
